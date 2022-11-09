6baeaa014152cfbd39ded747a34eb70d7eb63304
Saqib Ahmad <you@example.com>
2022-11-09 10:36:12 +0600

    second blog added

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Blogs = () => {
     return (
-        <section>
-            <h2 className='text-4xl font-semibold text-center mt-7 mb-5'>Blogs</h2>
+        <section className='mb-10'>
+            <h2 className='text-4xl font-semibold text-center mt-6 mb-8 underline'>Blogs</h2>
             <div className="blogs w-8/12 mx-auto">
                 {/* Blog-1 */}
-                <div className="blog-1 mb-7">
+                <div className="mb-12">
                     <h3 className='text-2xl text-center mb-2'><span className='font-semibold'>Asked:</span> Difference between SQL and NoSQL?</h3>
                     <p>
-                        <div>
+                        <div className='overflow-x-auto'>
                             <table className="table w-full">
 
                                 <thead>
@@ -48,6 +48,14 @@ const Blogs = () => {
                                 </tbody>
                             </table>
                         </div>
+                    </p>
+                </div>
+                {/* Blog-2 */}
+                <div className="mb-12">
+                    <h3 className='text-2xl text-center mb-2'><span className='font-semibold'>Asked:</span> What is JWT, and how does it work?</h3>
+                    <p>
+                        JWT Stand for Json Web Token. It is an open standard (RFC 7519) for securely transmitting information between parties as JSON object. It is compact, readable and digitally signed using a private key/ or a public key pair by the Identity Provider. <br /> <br />
+                        Basically the identity provider(IdP) generates a JWT certifying user identity and Resource server decodes and verifies the authenticity of the token using secret salt / public key.
                     </p>
                 </div>
             </div>
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
         <footer className="footer items-center p-4 bg-neutral text-neutral-content">
             <div className="items-center grid-flow-col">
                 <img src={img} alt="" />
-                <p>Copyright © 2022 - All right reserved</p>
+                <Link className="font-serif btn btn-ghost normal-case text-xl" to='/'>David's Burger</Link>
             </div>
             <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-                <Link className="font-serif btn btn-ghost normal-case text-xl" to='/'>David's Burger</Link>
+                <p>Copyright © 2022 - All right reserved</p>
             </div>
         </footer>
     );
