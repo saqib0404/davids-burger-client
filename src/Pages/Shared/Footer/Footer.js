ee38050f48a72161df848ef588ef2f2bc52c70d4
Saqib Ahmad <you@example.com>
2022-11-09 10:09:04 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import img from '../../../assets/footer.png'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+            <div className="items-center grid-flow-col">
+                <img src={img} alt="" />
+                <p>Copyright © 2022 - All right reserved</p>
+            </div>
+            <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+                <Link className="font-serif btn btn-ghost normal-case text-xl" to='/'>David's Burger</Link>
+            </div>
+        </footer>
     );
 };
 
