0ea04c49446d3f6911f965a8bc22efce2335e3d6
Saqib Ahmad <you@example.com>
2022-11-10 19:32:40 +0600

    update user implemented

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -5,6 +5,7 @@ import ReviewTable from '../ServiceDetails/ReviewTable';
 
 const MyReviews = () => {
     const { user } = useContext(AuthContext);
+    const [updateReviewId, setupdateReviewId] = useState('')
     const [reviews, setReviews] = useState([]);
 
     useEffect(() => {
@@ -26,7 +27,9 @@ const MyReviews = () => {
                 .then(data => {
                     console.log(data);
                     if (data.deletedCount > 0) {
-                        window.location.reload();
+                        const remaining = reviews.filter(review => review._id !== id)
+                        setReviews(remaining)
+                        // window.location.reload();
                         toast.success('Review deleted')
                     }
                 })
@@ -34,9 +37,58 @@ const MyReviews = () => {
         }
     }
 
+    // Update Review 
+    const handleEditReview = id => {
+        setupdateReviewId(id);
+    }
+    const handleUpdateReview = (e, id) => {
+        e.preventDefault();
+        const newReviewText = e.target.newReviewText.value;
+        const updateMsg = {
+            newReviewText
+        }
+
+        fetch(`http://localhost:5000/reviews/${updateReviewId}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updateMsg)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.matchedCount > 0) {
+                    toast.success('Review Updated')
+                    window.location.reload();
+                    // const remaining = orders.filter(odr => odr._id !== id);
+                    // const approving = orders.find(odr => odr._id === id)
+                    // approving.status = "Approved";
+
+                    // const newOrder = [approving, ...remaining];
+                    // setOrders(newOrder);
+                }
+            })
+    }
+
     return (
 
         <div className=' mb-10 mt-5  mx-6 md:mx-20 lg:mx-48 h-screen'>
+            {/* Modal */}
+            < input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal">
+                <form onSubmit={handleUpdateReview} className="modal-box relative">
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <br />
+                    <label className="label">
+                        <span className="label-text">Edit your review</span>
+                    </label>
+                    <input name='newReviewText' type="text" className="input input-bordered input-primary w-full" required />
+                    <div className='flex justify-end my-2'>
+                        <button type='submit' className='btn btn-primary'>Submit</button>
+                    </div>
+                </form>
+            </div>
             <Toaster></Toaster>
             {reviews.length < 1 ?
                 <h2 className='text-4xl mb-2 text-center font-semibold'>No reviews Were Added</h2>
@@ -59,6 +111,7 @@ const MyReviews = () => {
                                         key={review._id}
                                         author={true}
                                         handleDeleteReview={handleDeleteReview}
+                                        handleEditReview={handleEditReview}
                                         review={review}
                                     ></ReviewTable>)
                                 }
--- src/Pages/ServiceDetails/ReviewTable.js
+++ src/Pages/ServiceDetails/ReviewTable.js
@@ -1,33 +1,40 @@
 import React from 'react';
 
-const ReviewTable = ({ review, handleDeleteReview, author, }) => {
-    const { name, photo, reviewText, serviceName, serviceImg, serviceId } = review;
+const ReviewTable = ({ review, handleDeleteReview, author, handleEditReview }) => {
+    const { name, photo, reviewText, serviceName, serviceImg, serviceId, _id } = review;
+
 
 
     return (
-        <tr>
-            <td>
-                <div className="flex items-center space-x-3">
-                    <div className="avatar">
-                        <div className="mask mask-squircle w-12 h-12">
-                            <img src={author ? serviceImg : photo} alt="Avatar Tailwind CSS Component" />
+        <>
+
+
+
+            <tr>
+                <td>
+                    <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src={author ? serviceImg : photo} alt="Avatar Tailwind CSS Component" />
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">{author ? serviceName : name}</div>
                         </div>
                     </div>
-                    <div>
-                        <div className="font-bold">{author ? serviceName : name}</div>
-                    </div>
-                </div>
-            </td>
-            <td>{reviewText}</td>
-            {
-                author &&
-                <>
-                    <td><button className='btn btn-success'>Edit</button></td>
-                    <td><button onClick={() => handleDeleteReview(serviceId)} className='btn btn-error'>Delete</button></td>
+                </td>
+                <td>{reviewText}</td>
 
-                </>
-            }
-        </tr>
+                {
+                    author &&
+                    <>
+                        <td><label onClick={() => handleEditReview(_id)} htmlFor="my-modal-3" className='btn btn-success'>Edit</label></td>
+                        {/* <td><button className='btn btn-success'>Edit</button></td> */}
+                        <td><button onClick={() => handleDeleteReview(_id)} className='btn btn-error'>Delete</button></td>
+                    </>
+                }
+            </tr>
+        </>
     );
 };
 
